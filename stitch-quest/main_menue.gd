671c4106466389b793cc7ac455dc96be066087b7
Haneen Khalifa <haneenhany1205@gmail.com>
2024-10-04 18:50:41 +0300

    feat: Add main screens with basic integration

--- stitch-quest/main_menue.gd
+++ stitch-quest/main_menue.gd
@@ -13,7 +13,7 @@ func _process(delta: float) -> void:
 
 func _on_start_button_pressed() -> void:
 	#get_tree().change_scene_to_file("res://node_2d.tscn")
-	get_tree().change_scene_to_file("res://scenes/pause.tscn")
+	get_tree().change_scene_to_file("res://scenes/main_happy_stitch.tscn")
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()

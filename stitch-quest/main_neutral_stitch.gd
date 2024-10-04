44fda19366b51b039ba595d2a9e1d9d6312389a6
Haneen Khalifa <haneenhany1205@gmail.com>
2024-10-04 20:17:28 +0300

    feat: Include and handle background music
    Co-Authored-By: Rahma <122698447+rahma-khattab@users.noreply.github.com>

--- stitch-quest/main_neutral_stitch.gd
+++ stitch-quest/main_neutral_stitch.gd
@@ -2,8 +2,11 @@ extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	$AudioStreamPlayer2D.play()  # Start the audio
+
+func _on_audio_stream_player_2d_finished() -> void:
+	$AudioStreamPlayer2D.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,3 +16,9 @@ func _process(delta: float) -> void:
 
 func _on_pause_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/pause.tscn")
+
+func _on_mute_button_toggled(button_pressed):
+	if button_pressed:
+		$AudioStreamPlayer2D.volume_db = -80  # Mute the audio (set volume to minimum)
+	else:
+		$AudioStreamPlayer2D.volume_db = 0  # Unmute the audio (set volume back to normal)

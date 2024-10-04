867079e8f9caf23eaed54da2baba1bca92541113
Rahma <rahmakhattab07@gmail.com>
2024-10-04 17:25:47 +0300

    Added the resume/pause screen

--- stitch-quest/sprites/pause.gd
+++ stitch-quest/sprites/pause.gd
@@ -9,11 +9,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_start_button_pressed() -> void:
-	#get_tree().change_scene_to_file("res://node_2d.tscn")
-	get_tree().change_scene_to_file("res://scenes/pause.tscn")
-
+	
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
+
+func _on_resume_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://node_2d.tscn")

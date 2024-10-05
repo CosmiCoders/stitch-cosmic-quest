6b24b1da692e229939c3b24328fa6feb3b067e23
roaa.lotfy <roaa.m.lotfy@gmail.com>
2024-10-05 03:31:39 +0300

    quiz

---
+++ stitch-quest/scenes/quizz_1.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+var correct_answer = "Exoplanets"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Correct.visible = false
+
+# Handle when Button A is pressed
+func _on_button_a_pressed() -> void:
+	$corr.play()
+	$Correct.visible = true
+	$Wrong.visible = true
+	$Wrong2.visible = true
+	
+# Handle when Button B is pressed
+func _on_button_b_pressed() -> void:
+	$wrong.play()
+	$Wrong.visible = true
+# Handle when Button C is pressed
+func _on_button_c_pressed() -> void:
+	$wrong.play()
+	$Wrong2.visible = true
